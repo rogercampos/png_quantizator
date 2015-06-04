5dc5d4feed8877998a9a6df803ae1a3b328a6cc3
Roger Campos <roger@itnig.net>
2015-06-05 00:00:26 +0200

    Generalize tmp dir

--- spec/png_quantizator/image_spec.rb
+++ spec/png_quantizator/image_spec.rb
@@ -4,25 +4,32 @@ describe PngQuantizator::Image do
   let(:test_jpg) { File.join(File.dirname(__FILE__), "../data/sample.jpg") }
   let(:test_png) { File.join(File.dirname(__FILE__), "../data/sample.png") }
 
+  around do |block|
+    Dir.mktmpdir do |dir|
+      @tmpdir = dir
+      block.run
+    end
+  end
+
   describe "#quantize_to" do
     it "should not work with jpg files" do
       quantized = PngQuantizator::Image.new(test_jpg)
-      expect { quantized.quantize_to("/tmp/cucota.png") }.to raise_error(PngQuantizator::PngQuantError)
+      expect { quantized.quantize_to("#{@tmpdir}/cucota.png") }.to raise_error(PngQuantizator::PngQuantError)
     end
 
     it "should work with png files" do
       quantized = PngQuantizator::Image.new(test_png)
-      quantized.quantize_to("/tmp/cucota.png").should be_true
+      quantized.quantize_to("#{@tmpdir}/cucota.png").should be_true
 
-      File.file?("/tmp/cucota.png").should be_true
-      File.size("/tmp/cucota.png").should < File.size(test_png)
+      File.file?("#{@tmpdir}/cucota.png").should be_true
+      File.size("#{@tmpdir}/cucota.png").should < File.size(test_png)
 
-      FileUtils.rm_rf("/tmp/cucota.png")
+      FileUtils.rm_rf("#{@tmpdir}/cucota.png")
     end
   end
 
   describe "#quantize!" do
-    let(:original_file_path) { "/tmp/abracadabra.png" }
+    let(:original_file_path) { "#{@tmpdir}/abracadabra.png" }
 
     before do
       FileUtils.cp(test_png, original_file_path)
@@ -41,9 +48,9 @@ describe PngQuantizator::Image do
     end
 
     it "should not leave residual files in tmp" do
-      foo = `ls -1 /tmp/*.png | wc -l`
+      foo = `ls -1 #{@tmpdir}/*.png | wc -l`
       PngQuantizator::Image.new(original_file_path).quantize!
-      foo.should == `ls -1 /tmp/*.png | wc -l`
+      foo.should == `ls -1 #{@tmpdir}/*.png | wc -l`
     end
   end
 end
